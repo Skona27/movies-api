be4c74eae455263c114649a14e60a2650100c3a6
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-05-11 23:11:22 +0200

    D from CRUD

--- app/models/Movie.php
+++ app/models/Movie.php
@@ -22,26 +22,10 @@ class Movie extends Model {
         echo json_encode($result);
     }
 
-    public function create($movie) {
-        $insert = Database::instance()->query(
-            'INSERT INTO movies VALUES (:id, :title, :description, :year, :director, :language, :length, :rate)', [
-            'id' => 0,
-            'title' => $movie["title"],
-            'description' => $movie["description"],
-            'year' => $movie["year"],
-            'director' => $movie["director"],
-            'language' => $movie["language"],
-            'length' => $movie["length"],
-            'rate' => $movie["rate"]
-        ]);
-       
-        if(!$insert->error()) {
-            $result = Database::instance()->query(
-                'SELECT * FROM movies WHERE title= :title',
-                ['title' => $movie["title"]]
-            )->first();
-
-            echo json_encode($result);
-        }
+    public function delete($id) {
+        $result = Database::instance()->query(
+            'DELETE FROM movies WHERE id= :id', 
+            ['id' => $id]
+        );
     }
 }
